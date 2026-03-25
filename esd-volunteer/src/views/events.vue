396213a43414f3de126c1a8620367937f866ce4e
Tan Geok Jia <geokjia@gmail.com>
2026-03-25 16:13:13 +0800

    inital commit for vue

---
+++ esd-volunteer/src/views/events.vue
@@ -0,0 +1,11 @@
+<script>
+import EventsCard from '../components/eventsCard.vue';
+
+</script>
+
+<template>
+    <main>
+        <h2>TEST</h2>
+        <EventsCard></EventsCard>
+    </main>
+</template>
